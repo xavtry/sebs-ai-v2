2bc879b540bdf6d4cb906aa1d0fb714b99b2fe42
xavtry <sebastianishot68@gmail.com>
2025-10-27 16:37:46 -0400

    Update App.jsx

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,1 +1,11 @@
+import React from "react";
+import ChatWindow from "./components/ChatWindow";
+
+export default function App() {
+  return (
+    <div style={{ minHeight: "100vh", background: "#fff" }}>
+      <ChatWindow />
+    </div>
+  );
+}
 
